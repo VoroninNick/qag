01d0377a6fe0fe4c58d5526022bce763db912884
pasha <p.korenev@voroninstudio.eu>
2014-10-01 09:56:26 +0300

    install devise, rails_admin, ckeditor, paper_trail

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
   root to: 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.

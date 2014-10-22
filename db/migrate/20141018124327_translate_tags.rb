576018446f3691bf4fa9c25c0cdd732e19b98bad
pasha <p.korenev@voroninstudio.eu>
2014-10-22 21:48:50 +0300

    fix migration error

--- db/migrate/20141018124327_translate_tags.rb
+++ db/migrate/20141018124327_translate_tags.rb
@@ -1,14 +1,24 @@
 class TranslateTags < ActiveRecord::Migration
   def self.up
+    model = ActsAsTaggableOn::Tag
+
+    if model.respond_to?(:create_translation_table!)
+
+
     ActsAsTaggableOn::Tag.create_translation_table!({
         :name => :string
     }, {
         :migrate_data => true
     })
+
+    end
   end
 
   def self.down
-    ActsAsTaggableOn::Tag.drop_translation_table! :migrate_data => true
+    model = ActsAsTaggableOn::Tag
+    if model.respond_to?(:drop_translation_table!)
+      model.drop_translation_table! :migrate_data => true
+    end
   end
 end
 

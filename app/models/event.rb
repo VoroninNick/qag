2597bdd3b3cf28178ac0dea7dffbe0c3d6fa90db
VoroninNick <p.korenev@voroninstudio.eu>
2014-12-18 19:02:23 +0200

    images

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,7 +10,7 @@ class Event < ActiveRecord::Base
   attr_accessible :event_gallery_images, :event_gallery_image_id, :event_gallery_image_ids
 
 
-  accepts_nested_attributes_for :event_gallery_images
+  accepts_nested_attributes_for :event_gallery_images, :allow_destroy => true
   attr_accessible :event_gallery_images_attributes, :event_gallery_images
 
 

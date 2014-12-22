a02fa6304f1a8276ac03e5746d5ed293fc31e228
VoroninNick <p.korenev@voroninstudio.eu>
2014-12-22 21:00:59 +0200

    a

--- app/models/event.rb
+++ app/models/event.rb
@@ -44,8 +44,16 @@ class Event < ActiveRecord::Base
                         banner: "-quality 94 -interlace Plane",
                     }
 
+  has_attached_file :banner, :styles => { banner: '2100x500#' },
+                    :url  => "/assets/#{self.name.underscore}/:id/banner/:style/:basename.:extension",
+                    :path => ":rails_root/public/assets/#{self.name.underscore}/:id/expired_event_avatar/:style/:basename.:extension",
+                    convert_options: {
+                        banner: "-quality 94 -interlace Plane",
+                    }
+
   validates_attachment_file_name :avatar, :matches => [/png\Z/i, /jpe?g\Z/i, /gif\Z/i, /svg\Z/i]
   validates_attachment_file_name :banner, :matches => [/png\Z/i, /jpe?g\Z/i, /gif\Z/i, /svg\Z/i]
+  validates_attachment_file_name :expired_event_avatar, :matches => [/png\Z/i, /jpe?g\Z/i, /gif\Z/i, /svg\Z/i]
 
   #acts_as_taggable
 
@@ -53,7 +61,7 @@ class Event < ActiveRecord::Base
 
 
 
-  [:avatar, :banner].each do |paperclip_field_name|
+  [:avatar, :banner, :expired_event_avatar].each do |paperclip_field_name|
     attr_accessible paperclip_field_name.to_sym, "delete_#{paperclip_field_name}".to_sym, "#{paperclip_field_name}_file_name".to_sym, "#{paperclip_field_name}_file_size".to_sym, "#{paperclip_field_name}_content_type".to_sym, "#{paperclip_field_name}_updated_at".to_sym, "#{paperclip_field_name}_file_name_fallback".to_sym, "#{paperclip_field_name}_alt".to_sym
 
     attr_accessor "delete_#{paperclip_field_name}".to_sym
@@ -375,6 +383,19 @@ class Event < ActiveRecord::Base
         end
       end
       field :banner do
+        if asd = I18n.t("rails_admin.field_labels.#{method_name}", raise: true) rescue false
+          label asd
+        end
+        if type == :paperclip
+          model = @abstract_model.model_name.constantize
+          temp_instance = model.new
+          attr = temp_instance.send(method_name)
+          help help + ( attr.styles.map{|obj| info = obj[1]; res = {}; res[info.name.to_sym] = info.geometry; res  }).inspect
+
+        end
+      end
+
+      field :expired_event_avatar do
         if asd = I18n.t("rails_admin.field_labels.#{method_name}", raise: true) rescue false
           label asd
         end
--- db/migrate/20141222183711_add_expired_avatar_to_events.rb
+++ db/migrate/20141222183711_add_expired_avatar_to_events.rb
@@ -1,5 +1,20 @@
 class AddExpiredAvatarToEvents < ActiveRecord::Migration
-  def change
-    add_column :events, :expired_avatar, :has_attached_file
+  def up
+    #add_column :events, :expired_avatar
+
+    field_name = 'expired_event_avatar'
+    arr = [{:name=>"_file_name", :type=>:string}, {:name=>"_image_content_type", :type=>:string}, {:name=>"_file_size", :type=>:integer}, {:name=>"_updated_at", :type=>:datetime}, {:name=>"_file_name_fallback", :type=>:string}, {:name=>"_alt", :type=>:string}]
+    arr.each do |column|
+      add_column Event.table_name, "#{field_name}#{column[:name]}".to_sym, column[:type]
+    end
+
+  end
+
+  def down
+    field_name = 'expired_event_avatar'
+    arr = [{:name=>"_file_name", :type=>:string}, {:name=>"_image_content_type", :type=>:string}, {:name=>"_file_size", :type=>:integer}, {:name=>"_updated_at", :type=>:datetime}, {:name=>"_file_name_fallback", :type=>:string}, {:name=>"_alt", :type=>:string}]
+    arr.each do |column|
+      remove_column Event.table_name, "#{field_name}#{column[:name]}".to_sym, column[:type]
+    end
   end
 end

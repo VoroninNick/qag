959fac426e43639dd4f96a99c32929ee0c0a2dc4
VoroninNick <p.korenev@voroninstudio.eu>
2015-01-14 15:03:26 +0200

    fix

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -97,43 +97,48 @@
     var $fixed_header_top_container = $('#fixed-header-top-container')
     var $events_list_banner_image_wrapper = $('.events-list-banner-image-wrapper')
 
-    //var $header_menu_button_inner = $('#header-menu-button-inner')
-    var $header_menu_button_inner = $('#header-menu-button-label')
-    var min_scroll_top = $events_list_banner_image_wrapper.offset().top + $events_list_banner_image_wrapper.height() - 30;
-    $(window).scroll(function(){
-        //var min_scroll_top = 220
-        var $document = $(document)
-        var scroll_top = $document.scrollTop()
-        console.log("result: ", (scroll_top >= min_scroll_top) )
-        if(scroll_top >= min_scroll_top){
-            //$search_abs_row_wrapper.addClass('fixed')
-            $header_menu_button_inner.addClass('blue')
+//asd
+    if($events_list_banner_image_wrapper.length > 0) {
+        //var $header_menu_button_inner = $('#header-menu-button-inner')
+        var $header_menu_button_inner = $('#header-menu-button-label')
+        var min_scroll_top = $events_list_banner_image_wrapper.offset().top + $events_list_banner_image_wrapper.height() - 30;
+        $(window).scroll(function () {
+            //var min_scroll_top = 220
+            var $document = $(document)
+            var scroll_top = $document.scrollTop()
+            console.log("result: ", (scroll_top >= min_scroll_top))
+            if (scroll_top >= min_scroll_top) {
+                //$search_abs_row_wrapper.addClass('fixed')
+                $header_menu_button_inner.addClass('blue')
 
-        }
-        else{
-            //$search_abs_row_wrapper.removeClass('fixed')
-            $header_menu_button_inner.removeClass('blue')
-        }
-    })
+            }
+            else {
+                //$search_abs_row_wrapper.removeClass('fixed')
+                $header_menu_button_inner.removeClass('blue')
+            }
+        })
 
-    var $events_list = $('#events-list')
-    var $header_menu_button = $('#header-menu-button')
-    $events_list.on('mouseover', '.list-item', function(){
-        var $this = $(this)
-        var event_top = $this.offset().top;
-        var event_height = $this.height()
-        var  event_bottom = event_top + event_height
-        //var scrollTop = $(document).scrollTop()
-        var header_menu_button_top = $header_menu_button.offset().top
-        var header_menu_button_bottom = header_menu_button_top + $header_menu_button.height()
+        var $events_list = $('#events-list')
+        var $header_menu_button = $('#header-menu-button')
+        $events_list.on('mouseover', '.list-item', function () {
+            var $this = $(this)
+            var event_top = $this.offset().top;
+            var event_height = $this.height()
+            var event_bottom = event_top + event_height
+            //var scrollTop = $(document).scrollTop()
+            var header_menu_button_top = $header_menu_button.offset().top
+            var header_menu_button_bottom = header_menu_button_top + $header_menu_button.height()
 
-        if((header_menu_button_bottom >= event_top && header_menu_button_top <= event_bottom) || $fixed_header_top_container.hasClass('visible') ){
-            $header_menu_button_inner.removeClass('blue')
-        }
-        else{
-            $header_menu_button_inner.addClass('blue')
-        }
-    })
+            if ((header_menu_button_bottom >= event_top && header_menu_button_top <= event_bottom) || $fixed_header_top_container.hasClass('visible')) {
+                $header_menu_button_inner.removeClass('blue')
+            }
+            else {
+                $header_menu_button_inner.addClass('blue')
+            }
+        })
+    }
+
+//asd
 //}
 
 //$(document).on('page:load ready', function(){

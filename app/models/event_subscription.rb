0ee513d6b3469f24f345b6733a300edb33d5ced4
VoroninNick <p.korenev@voroninstudio.eu>
2015-01-16 18:38:49 +0200

    fixes

--- app/models/event_subscription.rb
+++ app/models/event_subscription.rb
@@ -62,10 +62,13 @@ class EventSubscription < ActiveRecord::Base
           user_id = bindings[:object].user_id
           #full_name = bindings[:view].full_name(user_id)
           #user_email = bindings[:object].user_id.to_s
-          user = User.find(user_id)
+          user = User.find(user_id) rescue nil
           #user_email =  (u =  User.find(user_id)) ? u.email : u.id.to_s
-          user_email = user.email
-          bindings[:view].link_to "#{user_email}", bindings[:view].rails_admin.show_path('user', user_email)
+          if user
+            bindings[:view].link_to "#{( e = user.email) ? e : user_id.to_s }", bindings[:view].rails_admin.show_path('user', user_email)
+          else
+            '-'
+          end
         end
       end
       field :event

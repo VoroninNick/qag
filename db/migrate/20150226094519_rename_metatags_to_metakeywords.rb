32aa10ccfea4da66628fc17a0001f829f1e96648
VoroninNick <p.korenev@voroninstudio.eu>
2015-02-26 11:58:56 +0200

    rename translation column

--- db/migrate/20150226094519_rename_metatags_to_metakeywords.rb
+++ db/migrate/20150226094519_rename_metatags_to_metakeywords.rb
@@ -2,5 +2,6 @@ class RenameMetatagsToMetakeywords < ActiveRecord::Migration
   def change
     model = VoroninStudio::PageMetadata
     rename_column model.table_name, :meta_tags, :meta_keywords
+    rename_column model.translation_class.table_name, :meta_tags, :meta_keywords
   end
 end

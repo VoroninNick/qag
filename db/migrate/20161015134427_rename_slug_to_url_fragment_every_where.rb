aa113930696ffd5f936c833f7eeb572cd2ad13b7
pasha <p.korenev@voroninstudio.eu>
2016-10-15 17:17:32 +0300

    fix

--- db/migrate/20161015134427_rename_slug_to_url_fragment_every_where.rb
+++ db/migrate/20161015134427_rename_slug_to_url_fragment_every_where.rb
@@ -1,8 +1,8 @@
 class RenameSlugToUrlFragmentEveryWhere < ActiveRecord::Migration
   def up
     tables.each do |table_name|
-      if columns(table_name).map{|c| c.name.to_s }.include?("url_fragment")
-        rename_column table_name, "url_fragment", :url_fragment
+      if columns(table_name).map{|c| c.name.to_s }.include?("slug")
+        rename_column table_name, "slug", :url_fragment
       end
     end
   end

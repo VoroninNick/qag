32b5bfede38d1c3c8172c79dbd037d1a5b5b399c
pasha <p.korenev@voroninstudio.eu>
2016-10-21 16:30:43 +0300

    fix

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -3,7 +3,7 @@ class MessageMailer < ActionMailer::Base
 		@message = message
 
 		default_to = 'p.korenev@voroninstudio.eu'
-		to = FormConfig.first.receiver_emails.split(',')
+		to = FormConfig.first.receiver_emails.split(',').map{|s| s.gsub("\r\n", "") }.select(&:present?)
 		if to.length == 0
 			to = default_to
 		end
@@ -14,7 +14,7 @@ class MessageMailer < ActionMailer::Base
 		@call_back = call_back
 
 		default_to = 'p.korenev@voroninstudio.eu'
-		to = FormConfig.second.receiver_emails.split(',')
+		to = FormConfig.second.receiver_emails.split(',').map{|s| s.gsub("\r\n", "") }.select(&:present?)
 		if to.length == 0
 			to = default_to
 		end

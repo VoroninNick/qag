513e9d7a4a124dab18c5ca38ee262474ce7705f5
pasha <p.korenev@voroninstudio.eu>
2016-10-26 20:10:01 +0300

    app

--- app/models/home_contact_info.rb
+++ app/models/home_contact_info.rb
@@ -42,6 +42,11 @@ class HomeContactInfo < ActiveRecord::Base
     end
   end
 
+  has_cache
+  def cache_instances
+    [HomePage.first]
+  end
+
   rails_admin do
     parent HomePage
     label I18n.t("rails_admin.model_labels.#{self.abstract_model.model_name.underscore}")
